2a025ce77ec8266618c0fb05b558a27acf6da984
hayatzidan <zidanhayat424@gmail.com>
2024-12-03 16:43:00 +0200

    added a 3 new files

---
+++ test.js
@@ -0,0 +1,7 @@
+document.getElementById('click').addEventListener('click', ()=> {
+    const ytyt = document.getElementById('ytyt').value;
+    const uuuu = document.getElementById('uuuu').value;
+
+
+    alert('Hello !, ' + ytyt + ' from ' + uuuu);
+});
